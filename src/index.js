f7cf685d9e1160af99b0e999c29fa4ed7e03a783
mstop4 <mstop4@gmail.com>
2018-04-23 18:32:11 -0400

    added gifme endpoint to search giphy

--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import morgan from 'morgan'
+import GphApiClient from 'giphy-js-sdk-core'
 
 dotenv.config()
+
 const app = express()
 app.use(morgan('short'))
+
+const client = GphApiClient(process.env.GIPHY_APIKEY)
 
 app.get('/', (req, res) => {
   res.send('Hello World! I am your new leader!')
 })
 
-app.get('/urls', (req, res) => {
+app.get('/json', (req, res) => {
   res.setHeader('Content-Type', 'application/json')
   res.send(JSON.stringify({ urls: [0] }))
 })
 
+app.get('/gifme', (req, res) => {
+  client.search('gifs', {'q': req.query.query})
+    .then((giphyRes) => {
+      res.setHeader('Content-Type', 'application/json')
+      res.send(JSON.stringify(giphyRes.data))
+    })
+    .catch((giphyErr) => {
+      let data = { error: giphyErr }
+      res.setHeader('Content-Type', 'application/json')
+      res.send(JSON.stringify(data))
+    })
+})
+
 // eslint-disable-next-line no-unused-vars
 const server = app.listen(process.env.SERVER_PORT, () => {
-  console.log('GIFcentration Server listening on port 3000')
-} )
+  console.log(`GIFcentration Server listening on port ${process.env.SERVER_PORT}`)
+})
--- test/index.test.js
+++ test/index.test.js
@@ -12,9 +12,9 @@ describe('Main Page', () => {
   })
 })
 
-describe('Url endpoint', () => {
+describe('/json endpoint', () => {
   it('should get a JSON object', (done) => {
-    request(`http://localhost:${process.env.SERVER_PORT}/urls`, (error, res, body) => {
+    request(`http://localhost:${process.env.SERVER_PORT}/json`, (error, res, body) => {
 
       let json = JSON.parse(body)
       expect(json).to.be.an('object')
@@ -23,10 +23,21 @@ describe('Url endpoint', () => {
   })
 
   it('the JSON object should have the key "urls", which has an array as a value', (done) => {
-    request('http://localhost:3000/urls', (error, res, body) => {
+    request('http://localhost:3000/json', (error, res, body) => {
 
       let json = JSON.parse(body)
       expect(json.urls).to.be.an('array')
+      done()
+    })
+  })
+})
+
+describe('/gifme endpoint', () => {
+  it('should get search results for "cats" as an array', (done) => {
+    request(`http://localhost:${process.env.SERVER_PORT}/gifme?query=cats`, (error, res, body) => {
+
+      let json = JSON.parse(body)
+      expect(json).to.be.an('array')
       done()
     })
   })
