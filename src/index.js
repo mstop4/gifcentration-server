cb780e6425a828615c7ac92c0053ba456a98e874
mstop4 <mstop4@gmail.com>
2018-04-23 16:47:05 -0400

    added morgan

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
+import morgan from 'morgan'
 
 const app = express()
+app.use(morgan('short'))
 
 app.get('/', (req, res) => {
   res.send('Hello World! I am your new leader!')
