d92fc4bbfffe1d06aa72586f6c62317d4e12be3e
mstop4 <mstop4@gmail.com>
2018-04-23 16:37:03 -0400

    better server scripts

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! I am your new leader!')
 })
 
+// eslint-disable-next-line no-unused-vars
 const server = app.listen(3000, () => {
   console.log('GIFcentration Server listening on port 3000')
 } )
