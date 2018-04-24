316552ec347585d94ed3afb738d27d4b252758eb
mstop4 <mstop4@gmail.com>
2018-04-24 14:22:17 -0400

    tweaked debug gallery

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ app.get('/gifme/:format', (req, res) => {
       for (let i = 0; i < giphyRes.data.length; i++) {
         let url = null
         
+        // return GIFs with less than 200px height and width
         if (parseInt(giphyRes.data[i].images.fixed_width.height) > 200) {
           url = `https://media.giphy.com/media/${giphyRes.data[i].images.media_id}/200.gif`
         } else {
