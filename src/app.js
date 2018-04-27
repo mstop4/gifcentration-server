d1dbb83f86538373efca73710f765c4dc0ab35ad
mstop4 <mstop4@gmail.com>
2018-04-27 16:18:01 -0400

    updated .env.example

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ const app = express()
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
