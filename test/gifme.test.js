5996485c25caff68db41a28be29fe962bb1f45bb
mstop4 <mstop4@gmail.com>
2018-04-27 16:21:47 -0400

    updated tests to reflect new ap structure

---
+++ test/gifme.test.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+let chai = require('chai')
+let expect = chai.expect
+let request = require('request')
+
+describe('/gifme endpoint', () => {
+
+  it('should get search results for "cats" as an array', (done) => {
+    request(`http://localhost:${process.env.SERVER_PORT}/gifme/json?query=cats`, (error, res, body) => {
+
+      let json = JSON.parse(body)
+      expect(json).to.be.an('array').that.is.not.empty.and.does.not.include('error')
+      done()
+    })
+  })
+
+  it('a blank query should return an empty array', (done) => {
+    request(`http://localhost:${process.env.SERVER_PORT}/gifme/json?query=`, (error, res, body) => {
+
+      let json = JSON.parse(body)
+      expect(json).to.be.an('array').that.is.empty
+      done()
+    })
+  })
+
+  it('a missing query should still return some results', (done) => {
+    request(`http://localhost:${process.env.SERVER_PORT}/gifme/json`, (error, res, body) => {
+
+      let json = JSON.parse(body)
+      expect(json).to.be.an('array').that.is.not.empty.and.does.not.include('error')
+      done()
+    })
+  })
+
+  it('should fetch 50 dog GIFs', (done) => {
+    request(`http://localhost:${process.env.SERVER_PORT}/gifme/json?query=dog&limit=50`, (error, res, body) => {
+
+      let json = JSON.parse(body)
+      expect(json).to.be.an('array').that.has.lengthOf(50)
+      done()
+    })
+  })
+})
