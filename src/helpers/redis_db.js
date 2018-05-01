689131015afed4a85901babc793725cc5f5a4bab
mstop4 <mstop4@gmail.com>
2018-05-01 11:04:19 -0400

    added redis config to env

--- src/helpers/redis_db.js
+++ src/helpers/redis_db.js
@@ -1,6 +1,8 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import redis from 'redis'
 
-const client = redis.createClient()
+const client = redis.createClient(process.env.REDIS_URL, {no_ready_check: true})
 
 client.on('connect', () => {
   console.log('Redis connected on port 6379')
